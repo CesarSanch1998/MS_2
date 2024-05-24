08f67e6e39764238b6e9b1eed45f28935eb77bd0
CesarSanch1998 <cs96548@gmail.com>
2024-05-24 12:47:15 -0400

    returned

--- scripts/update_db_request.py
+++ scripts/update_db_request.py
@@ -8,22 +8,22 @@ def update_client_send(data):
     if request == None:
         return "Cliente no econtrado en la db"
     else:
-        session.query(client_db).filter(client_db.contract == data.contract).update(data.dict())
+        # session.query(client_db).filter(client_db.contract == data.contract).update(data.dict())
         #UPDATE DATA IN POSGRESTSQL DEFINITIOS
-        # request.frame = data.frame
-        # request.slot = data.slot
-        # request.port  = data.port
-        # request.onu_id = data.onu_id
-        # request.olt = data.olt
-        # request.fsp = data.fsp
-        # request.fspi = data.fspi
-        # request.name_1 = data.name_1
-        # request.name_2 = data.name_2
-        # request.state = data.state
-        # request.sn = data.sn
-        # request.device = data.device
-        # request.plan_name = data.plan_name
-        # request.spid = data.spid
+        request.frame = data.frame
+        request.slot = data.slot
+        request.port  = data.port
+        request.onu_id = data.onu_id
+        request.olt = data.olt
+        request.fsp = data.fsp
+        request.fspi = data.fspi
+        request.name_1 = data.name_1
+        request.name_2 = data.name_2
+        request.state = data.state
+        request.sn = data.sn
+        request.device = data.device
+        request.plan_name = data.plan_name
+        request.spid = data.spid
 
         #Mandar valores a guardar
         session.add(request)
