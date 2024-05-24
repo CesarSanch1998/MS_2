d625ad629f8f00e45e3689e140be8e1cb925de88
CesarSanch1998 <cs96548@gmail.com>
2024-05-24 14:36:58 -0400

    add contract

--- scripts/update_db_request.py
+++ scripts/update_db_request.py
@@ -6,10 +6,12 @@ def update_client_send(data):
     # print(data.contract)
     request = session.query(client_db).filter(client_db.contract == data.contract).first()
     if request == None:
+        add_client_db(data)
         return "Cliente no econtrado en la db"
     else:
         # session.query(client_db).filter(client_db.contract == data.contract).update(data.dict())
         #UPDATE DATA IN POSGRESTSQL DEFINITIOS
+        request.contract = data.contract
         request.frame = data.frame
         request.slot = data.slot
         request.port  = data.port
@@ -30,3 +32,27 @@ def update_client_send(data):
         session.commit()
         session.refresh(request) 
     return 'Client Update Successfully'
+
+def add_client_db(data):
+    client = client_db(contract=data['onu_contract'],
+                       frame=data.frame,
+                       slot=data.slot,
+                       port=data.port,
+                       onu_id=data.onu_id,
+                       olt=data.olt,
+                       fsp=data.fsp,
+                       fspi=data.fspi,
+                       name_1=data.name_1,
+                       name_2=data.name_2,
+                       state=data.state,
+                       sn=data.sn,
+                       device=data.device,
+                       plan_name = data.plan_name,
+                       spid = data.spid)
+    
+    
+    #Mandar valores a guardar
+    session.add(client)
+    session.commit()
+     
+    return 'Client Add Successfully'
