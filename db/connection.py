e462426960a50b9d66599aa91e4ed6457ae8f32d
CesarSanch1998 <cs96548@gmail.com>
2024-05-30 14:01:54 -0400

    debug mode active

--- db/connection.py
+++ db/connection.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 engine = create_engine(f'postgresql+psycopg2://{os.environ["DB_USER"]}:{os.environ["DB_PASSWORD"]}@{os.environ["DB_HOST"]}:5432/{os.environ["DB_NAME"]}')
-# engine.echo = True 
+engine.echo = True 
 conn = engine.connect()
 
 Session = sessionmaker(engine)
